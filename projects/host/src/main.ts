6f47c0d1840d428419e101b41cc36a87a3a64b0e
Evan Scott <scottem3@mymail.vcu.edu>
2023-10-28 08:51:53 -0400

    feat: add module federation to host (dynamic with manifest.json)

--- projects/host/src/main.ts
+++ projects/host/src/main.ts
@@ -1,7 +1,6 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { initFederation } from '@angular-architects/module-federation';
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+initFederation('/assets/mf.manifest.json')
+  .catch(err => console.error(err))
+  .then(_ => import('./bootstrap'))
   .catch(err => console.error(err));
